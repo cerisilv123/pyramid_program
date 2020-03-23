6aa8337fc22629694e121298ade08c7c6c4149b8
cerisilv123 <60038915+cerisilv123@users.noreply.github.com>
2020-03-23 12:16:24 +0000

    added a descending pyramid with a menu to choose between ascending and descending

--- main.cpp
+++ main.cpp
@@ -11,24 +11,57 @@ int main () {
     
     size_t rows {};
     rows = letters_input_list.length(); // Calculating rows
-            
-    size_t r {};
-    size_t c {};
-    size_t sp {};
-
-    for (r = 1; r <= rows; r++) { // Outer loop - Loop through number of rows - ABC = 3 Rows
-        for (sp = 1; sp <= rows-r; sp++) { // Inner loop 1 - white space before - Loop 1: (3-1) = 2
-            cout << " ";
-        }
-        for (c = 1; c <= r; c++) { // Inner loop 2 - print out letters to pyramid
-            cout << letters_input_list.at(c-1); // will print element at index (1-1).. (1-1) + (2-1).. (1-1) + (2-1) + (3-1)
-        }
-        for (size_t k = c-2; k >= 1; k--) { // Inner loop 3 - Print out descending letters
-            cout << letters_input_list.at(k-1);
-        }
-        cout << "\n"; // At end of inner loops it will print a new line
-    }
     
+    char selection {}; // Menu selection
+            
+    size_t r {}; // Loop through rows
+    size_t c {}; // Print out letters before descending
+    size_t sp {}; // White space
+    
+    do {
+        
+      cout << "\n======== Menu ========" << endl; 
+      cout << "\nA - Ascending Pyramid" << endl; 
+      cout << "D - Descending Pyramid" << endl; 
+      cout << "Q - Quit Program" << endl; 
+      cin >> selection;
+    
+     if (selection == 'A' || selection == 'a') {
+      for (r = 1; r <= rows; r++) { // Ascending Pyramid - Outer loop - Loop through number of rows 
+          for (sp = 1; sp <= rows-r; sp++) { // Inner loop 1 - white space before 
+            cout << " ";
+          }
+          for (c = 1; c <= r; c++) { // Inner loop 2 - print out letters to pyramid
+            cout << letters_input_list.at(c-1); 
+          }
+          for (size_t k = c-2; k >= 1; k--) { // Inner loop 3 - Print out descending letters
+            cout << letters_input_list.at(k-1);
+          }
+          cout << "\n"; // At end of inner loops it will print a new line
+      }
+     }
+     else if (selection == 'D' || selection == 'd') {
+      for (r = rows; r > 0; r--) { // Descending Pyramid - Outer loop - Loop through number of rows 
+          for (sp = 0; sp<rows-r ;sp++) { // Inner loop 1 - white space before 
+             cout << " "; 
+          }
+          for (c = 1; c <= r; c++) { // Inner loop 2 - print out letters to pyramid
+             cout << letters_input_list.at(c-1);    
+          } 
+          for (size_t k = c-2; k >= 1; k--) { // Inner loop 3 - Print out descending letters
+             cout << letters_input_list.at(k-1);
+          }
+          cout << "\n"; // At end of inner loops it will print a new line
+      } 
+     }
+     else if (selection == 'q' || selection == 'Q') {
+        cout << "\nProgram Quit" << endl;
+     }
+     else {
+        cout << "\nUser input error, please try again" << endl; 
+     }
+    } while (selection != 'q' && selection != 'Q');
+      
   cout << endl; 
   return 0;   
 } 
